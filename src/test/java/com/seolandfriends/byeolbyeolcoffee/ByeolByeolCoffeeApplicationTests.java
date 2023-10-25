b27bd3f8d20c321aedcdfe423415c6aceb000ca2
phj9745 <1022phj@naver.com>
2023-10-26 02:43:40 +0900

    init

---
+++ src/test/java/com/seolandfriends/byeolbyeolcoffee/ByeolByeolCoffeeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seolandfriends.byeolbyeolcoffee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ByeolByeolCoffeeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
