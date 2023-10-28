e02aa5db507dddf898c7fb672d0745e0468cd5af
phj9745 <1022phj@naver.com>
2023-10-29 02:01:17 +0900

    feat(phj9745): 레시피 엔티티 추가
    레시피 엔티티와 vo를 추가했습니다.
    
    Closes #3

---
+++ src/main/java/com/seolandfriends/byeolbyeolcoffee/recipe/command/domain/aggregate/vo/CustomOption.java
@@ -0,0 +1,17 @@
+package com.seolandfriends.byeolbyeolcoffee.recipe.command.domain.aggregate.vo;
+
+import java.io.Serializable;
+
+import javax.persistence.Embeddable;
+
+import lombok.NoArgsConstructor;
+
+@Embeddable
+@NoArgsConstructor
+public class CustomOption implements Serializable {
+	private Long customOptionId;
+
+	public CustomOption(Long customOptionId) {
+		this.customOptionId = customOptionId;
+	}
+}
