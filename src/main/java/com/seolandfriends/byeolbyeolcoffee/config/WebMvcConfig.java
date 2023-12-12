dc56aca652fc921d08b9724aac75db083b5047b9
phj9745 <1022phj@naver.com>
2023-12-13 01:22:06 +0900

    cors 에러 수정

--- src/main/java/com/seolandfriends/byeolbyeolcoffee/config/WebMvcConfig.java
+++ src/main/java/com/seolandfriends/byeolbyeolcoffee/config/WebMvcConfig.java
@@ -20,12 +20,4 @@ public class WebMvcConfig implements WebMvcConfigurer {
 		registry.addResourceHandler(ADD_RESOURCE_HANDLER)
 			.addResourceLocations(ADD_RESOURCE_LOCATION);
 	}
-
-	public void addCorsMappings(CorsRegistry registry) {
-		registry.addMapping("/**") // 또는 특정 경로 예: "/recipes/**"
-			.allowedOrigins("http://localhost:3000") // 또는 "*"로 모든 출처 허용
-			.allowedMethods("GET", "POST", "PUT", "DELETE") // 등등.
-			.allowedHeaders("*")
-			.allowCredentials(true);
-	}
 }
