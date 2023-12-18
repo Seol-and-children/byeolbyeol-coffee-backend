f8822edc7e289bb4d640aa909c32054d1f391831
phj9745 <1022phj@naver.com>
2023-12-18 21:31:16 +0900

    cascade 설정 수정

--- src/main/java/com/seolandfriends/byeolbyeolcoffee/recipe/command/domain/aggregate/entity/RecipeComment.java
+++ src/main/java/com/seolandfriends/byeolbyeolcoffee/recipe/command/domain/aggregate/entity/RecipeComment.java
@@ -27,7 +27,7 @@ public class RecipeComment {
 	@Column(name = "id", nullable = false)
 	private Long commentId;
 
-	@ManyToOne(cascade = CascadeType.PERSIST)
+	@ManyToOne
 	@JoinColumn(name = "user_id", nullable = false)
 	private CommentUserVO commentUserVO;
 
